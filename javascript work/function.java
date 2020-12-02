04a00ae458a0d07f230c595d750e774da9892b14
PiturcaStef <piturcastef@yahoo.com>
2020-12-02 14:05:39 +0200

    functie

---
+++ javascript work/function.java
@@ -0,0 +1,8 @@
+function sayhi(name,age){
+    document.write("<h1>Hello"+ name+ "</h1>");
+    document.write("<p>You are " + age + "</p>")
+  }
+  sayhi(' Steve', 34);
+  sayhi(' Dave',23);
+  sayhi('Tom',80);
+  
