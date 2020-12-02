c982121ff02ae7e18a7ed524b40a41c72ca7d7ce
PiturcaStef <piturcastef@yahoo.com>
2020-12-02 20:46:18 +0200

    message

--- Script1.js
+++ Script1.js
@@ -3,7 +3,7 @@ var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
 
 button.addEventListener("click", function(){
-    var newmessage = document.createElement("li");
+    var Newmessages = document.createElement("<li>");
     newmessage.innerHTML = textbox.nodeValue;
     messages.appendChild(newmessage);
 });
