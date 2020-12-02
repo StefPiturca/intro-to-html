30ed500218fa4d9f740398e748fc1a7b34d33d8c
PiturcaStef <piturcastef@yahoo.com>
2020-12-02 17:12:33 +0200

    modificari

---
+++ Script1.js
@@ -0,0 +1,16 @@
+class Script1 {
+
+    /**
+     * @param args;     the command line arguments
+     * @param document;
+     */
+     {
+    var message = document.getElementById("messages");
+    var textbox= document.getElementById("textbox");
+     var button = document.getElementById("button");
+    button.addEventListener("click",function(){
+        var newmessage = document.createElement("li");
+       newmessage.innerHTML = textbox.nodeValue;
+       message.appendChild(newmessage);
+    });
+    }
