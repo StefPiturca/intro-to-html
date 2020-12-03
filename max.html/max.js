53ff5b1fbbeb2f90fc1e99ce952f6775eda9902f
PiturcaStef <piturcastef@yahoo.com>
2020-12-03 12:18:28 +0200

    change

---
+++ max.html/max.js
@@ -0,0 +1,9 @@
+function max(num1,num2,num3);{
+      if(num1 >= num2 & num1 >= num3);{
+      return num1;
+      else
+      return num2;
+      else
+      return num3
+
+document.write(max(3,4,5);
